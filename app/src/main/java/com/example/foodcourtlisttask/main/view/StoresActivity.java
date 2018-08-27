deee5bbdef881494ee718dcf8e4c7d66e93ace2c
Aya <ayahilal16@gmail.com>
2018-08-27 13:13:46 +0200

    splash screen and application structure

---
+++ app/src/main/java/com/example/foodcourtlisttask/main/view/StoresActivity.java
@@ -0,0 +1,17 @@
+package com.example.foodcourtlisttask.main.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class StoresActivity extends AppCompatActivity{
+
+
+}
